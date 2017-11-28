decd6f332e307b6e060e010a1a6dee313b77dfd7
iHassan <hshabbir94@yahoo.com>
2017-11-29 04:02:57 +0500

    Updated redirect url

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -379,7 +379,8 @@ class Auth extends CI_Controller
 		{
 			// redirect them to the auth page
 			$this->session->set_flashdata('message', $this->ion_auth->messages());
-			redirect("auth", 'refresh');
+			//redirect("auth", 'refresh');
+			redirect("app/activation-success", 'refresh');
 		}
 		else
 		{
