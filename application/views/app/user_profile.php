43b85d8ef15b9d2893f2702b5d79de45b7e485f7
iHassan <hshabbir94@yahoo.com>
2017-12-16 21:41:10 +0500

    Added 'edit user profile' functionality

---
+++ application/views/app/user_profile.php
@@ -0,0 +1,104 @@
+
+<!-- ======================================
+        User Profile
+===========================================-->
+
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="header">
+                                <h3>Edit Profile</h3>
+                            </div>
+                               <div class="content"> 
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-3 form-group">
+                                                <label>Username</label>
+                                                <input type="text" class="form-control" placeholder="Username" value="michael23">
+                                        </div>
+                                        <div class="col-md-4 form-group">                                           
+                                                <label for="exampleInputEmail1">Email address</label>
+                                                <input type="email" class="form-control" placeholder="Email">
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6 form-group">                                            
+                                                <label>First Name</label>
+                                                <input type="text" class="form-control" placeholder="Company" value="Mike">
+                                        </div>
+                                        <div class="col-md-6 form-group">                                           
+                                                <label>Last Name</label>
+                                                <input type="text" class="form-control" placeholder="Last Name" value="Andrew">                                            
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            
+                                                <label>Address</label>
+                                                <input type="text" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4 form-group">                                          
+                                                <label>City</label>
+                                                <input type="text" class="form-control" placeholder="City" value="Mike">                                          
+                                        </div>
+                                        <div class="col-md-4 form-group">                                          
+                                                <label>Country</label>
+                                                <input type="text" class="form-control" placeholder="Country" value="Andrew">                                            
+                                        </div>
+                                        <div class="col-md-4 form-group">
+                                                <label>Postal Code</label>
+                                                <input type="number" class="form-control" placeholder="ZIP Code"> 
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            
+                                                <label>About Me</label>
+                                                <textarea rows="5" class="form-control" placeholder="Here can be your description" value="Mike">Lorem Ipsum dummy text Lorem Ipsum dummy text.</textarea>
+                                            
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
+                                    <div class="clearfix"></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card card-user">
+                            <div class="image">
+                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&amp;fm=jpg&amp;h=300&amp;q=75&amp;w=400" alt="...">
+                            </div>
+                            <div class="content">
+                                <div class="author">
+                                     <a href="#">
+                                    <img class="avatar border-gray" src="<?php echo ASSETS_URL ?>images/face-1.jpg" alt="...">
+
+                                      <h4 class="title">Mike Andrew<br>
+                                         <small>michael24</small>
+                                      </h4>
+                                    </a>
+                                </div>
+                                <p class="user-description text-center"> "Lorem Ipsum dummy text Lorem Ipsum dummy text"
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+    
+
+<!-- ======================================
+        User Profile
+===========================================-->
